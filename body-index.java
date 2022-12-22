90ecff9659c302a34d473b6a79455ddaaa48803a
bahri <109550978+Bahri20@users.noreply.github.com>
2022-12-22 19:00:42 +0300

    Create body-index.java

---
+++ body-index.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+        public static void main(String[] args) {
+                Scanner input=new  Scanner(System.in);
+                double kilo, boy, indeks;
+                System.out.print("kilonu gir: ");
+                kilo= input.nextDouble();
+                System.out.print("boyunu metre cinsinden gir: ");
+                boy= input.nextDouble();
+
+                indeks=kilo/(boy*boy);
+                System.out.println("vücut kitle indeksiniz: " + indeks);
+
+        }
+}
