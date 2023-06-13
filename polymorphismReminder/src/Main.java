828b559370326dd0cabaf4283e65c6f94f3ca842
bahri <109550978+Bahri20@users.noreply.github.com>
2023-06-14 02:20:25 +0300

    Update Main.java

--- polymorphismReminder/src/Main.java
+++ polymorphismReminder/src/Main.java
@@ -1,18 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         CostumerData costumerData=new CostumerData(new EmailData());
-        costumerData.add();
-
-
-
-
-
-
-
-
-
-
-
-        
+        costumerData.add();    
     }
 }
